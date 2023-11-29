e27930cee41f976b7652bc828866f7b916b4ff4d
Gabriel <gabriel.forcellini@hotmail.com>
2023-11-28 22:49:56 -0300

    Adicionado função que busca simbolo na tabela de simbolos

--- semantico.py
+++ semantico.py
@@ -2,12 +2,22 @@ class AnaliseSemantica:
     def __init__(self):
         self.tabela_simbolos = {}
     
+    # Adiciona um simbolo na tabela de simbolos
     def adicionar_simbolo(self, nome, categoria, tipo, nivel):
         if (nome and nivel) not in self.tabela_simbolos:
             self.tabela_simbolos[nome] = {'Categoria': categoria, 'Tipo': tipo, 'Nivel': nivel}
         else:
             print(f"Símbolo '{nome}' já existe na tabela de símbolos.")
+    
+    # Busca um simbolo na tabela de simbolos, retornando True quando encontrado
+    # e False quando não encontrado.
+    def busca_simbolo(self, nome, categoria, tipo, nivel):
+        if (nome and nivel) not in self.tabela_simbolos:
+            return True
+        
+        return False
 
+    # Faz a remoção de um simbolo pelo seu nome.
     def remover_simbolo(self, nome):
         if nome in self.tabela_simbolos:
             del self.tabela_simbolos[nome]
@@ -15,6 +25,7 @@ class AnaliseSemantica:
         else:
             print(f"Símbolo '{nome}' não encontrado na tabela de símbolos.")
 
+    # Imprime a tabela - Apenas para desenvolvimento
     def visualizar_tabela(self):
         print("Tabela de Símbolos:")
         for nome, info in self.tabela_simbolos.items():
